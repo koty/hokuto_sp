3efdc10c2f74c6c0a8b548a323cf178fa3636a7e
koty <kouichi.nishizawa+github@gmail.com>
2016-10-18 18:14:19 +0900

    1st commit

---
+++ hokuto_sp/hokuto_sp/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace hokuto_sp
+{
+	public class Rec: INotifyPropertyChanged
+	{
+		public string Name { get; set; }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+	public class MainPageViewModel: INotifyPropertyChanged
+	{
+		public ObservableCollection<Rec> Recs { get; private set; }
+		public MainPageViewModel()
+		{
+			this.Recs = new ObservableCollection<Rec>(new [] {
+				new Rec() { Name = "にゃー"},
+				new Rec() { Name = "わんわん"},
+				new Rec() { Name = "ヒヒーン"}
+			});
+			this.MyName = "こんにちは。";
+		}
+		public string MyName { get; private set; }
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
